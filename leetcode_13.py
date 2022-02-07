3f502c335dc974ac38e23032c7113d04df94bb03
suvi#007 <89591798+SuvigyaPandey@users.noreply.github.com>
2022-02-07 21:01:18 +0530

    Create leetcode_13.py

---
+++ leetcode_13.py
@@ -0,0 +1,22 @@
+##Link for the question - https://leetcode.com/problems/roman-to-integer/
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        dic={'I': 1, 'V':5, 'X':10,'L':50,'C':100,'D':500,'M':1000}
+        
+        i=0
+        num=0
+        while i<len(s)-1:
+            
+            if dic[s[i]]>=dic[s[i+1]]:
+                num+=(dic[s[i]])
+                i+=1
+            else:
+                num -=(dic[s[i]])
+                i+=1
+        num += dic[s[len(s)-1]]
+        return num
+    
+    
+        
