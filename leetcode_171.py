b85c16b8ec0f14590924cc5cb8e4ef796949501e
suvi#007 <89591798+SuvigyaPandey@users.noreply.github.com>
2022-02-22 22:09:00 +0530

    Create leetcode_171.py

---
+++ leetcode_171.py
@@ -0,0 +1,13 @@
+# link for code - https://leetcode.com/problems/excel-sheet-column-number/
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        
+        result = 0
+        for c in columnTitle:
+            d = ord(c)-ord('A')+1
+            
+            result = result*26+d
+            
+        return result
